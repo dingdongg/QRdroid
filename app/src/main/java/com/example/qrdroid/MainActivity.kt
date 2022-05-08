fcdb521740cfa04c0562e44de04ddb87647e3285
donggyu <donggyu258@gmail.com>
2022-05-07 23:35:55 -0700

    Created method stubs for each of the buttons in MainActivity

--- app/src/main/java/com/example/qrdroid/MainActivity.kt
+++ app/src/main/java/com/example/qrdroid/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.qrdroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Transitions into ScanActivity via an Intent, open device's camera
+     */
+    fun scanningPage(view: View) {
+    }
+
+    /**
+     * Transitions into BookmarksActivity via an Intent
+     */
+    fun bookmarksPage(view: View) {
+
+    }
+
+    /**
+     * Toggle between light and dark mode when the switch is clicked
+     */
+    fun toggleDarkMode() {
+
     }
 }
