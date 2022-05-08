51125814babe2883955efdffd78af88862d0ffc8
donggyu <donggyu258@gmail.com>
2022-05-08 00:40:22 -0700

    implemented dark mode toggle functionality

--- app/src/main/java/com/example/qrdroid/MainActivity.kt
+++ app/src/main/java/com/example/qrdroid/MainActivity.kt
@@ -3,11 +3,25 @@ package com.example.qrdroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switch: Switch = findViewById(R.id.switch1)
+
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+        }
     }
 
     /**
@@ -20,13 +34,6 @@ class MainActivity : AppCompatActivity() {
      * Transitions into BookmarksActivity via an Intent
      */
     fun bookmarksPage(view: View) {
-
-    }
-
-    /**
-     * Toggle between light and dark mode when the switch is clicked
-     */
-    fun toggleDarkMode() {
 
     }
 }
